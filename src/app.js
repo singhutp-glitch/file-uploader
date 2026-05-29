8f4b5865b1aa420db8f99d60c3c4ebe26cf06026
singhutp-glitch <singhutp@gmail.com>
2026-05-29 18:59:01 +0530

    Add script and PORT

--- src/app.js
+++ src/app.js
@@ -72,6 +72,9 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+
+const PORT = process.env.PORT || 3000 ;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
